2e05b895851ad8434177957be165ec6b3f51d7b7
alvaro <aejjimenez19@outlook.com>
2024-06-06 21:04:57 -0500

    feat: Creacion de los modelos

---
+++ src/app/shared/Models/model.ts
@@ -0,0 +1,45 @@
+export class usuario {
+    id?:number;
+    apellido?:String;
+    cedula?:String;
+    correo?:String;
+    departamento?:String;
+    direccion?:String;
+    fecha?:String;
+    municipio?:String;
+    nombre?:String;
+    telefono?:String;
+}
+
+
+export class comercio {
+    id?:number;
+    int?:number;
+    departamento?:String;
+    direccion?:String;
+    municipio?:String;
+    razon?:String;
+    telefono?:String;
+}
+
+export class factura {
+   id?:number;
+   fecha?:String;
+   iva?:number;
+   numeroFactura?:number;
+   subtotal?:number;
+   total?:number;
+   comercio?:comercio;
+   usuario?:usuario;
+   productos?:producto[];
+}
+
+
+export class producto {
+    id?:number;
+    cantidad?:number;
+    nombre?:String;
+    total?:number;
+    totaliva?:number;
+    factura?:number;
+}
